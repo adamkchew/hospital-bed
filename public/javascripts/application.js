e5521c486fcdc4fe3cd8aa622e3f14d44ad81985
Adam Chew <adamkchew@gmail.com>
2017-07-29 11:45:10 -0400

    Adding routes and removing 404's and 302's

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,31 @@
-var app = angular.module('website', []);
+//Global elements the needs to be loaded once
+global.jQuery = require('jquery');
+global.bootstrap = require('bootstrap');
+
+//Angular dependencies
+var angular = require('angular');
+var angularRoute = require('angular-route');
+var angularfire = require('angularfire');
+
+//Utilities
+var moment = require('moment');
+
+var app = angular.module('website', [angularRoute, angularfire]);
 
 //Configurations
-app.config(function($locationProvider) {
+app.config(function($locationProvider, $routeProvider) {
 
   //Hashbang for Google SEO
   $locationProvider.html5Mode(true).hashPrefix('!');
 
-});
+  //Routes
+  $routeProvider.when('/', {
+    templateUrl: 'views/index.html'
+  }).when('/control', {
+    templateUrl: 'views/control.html'
+  }).otherwise({
+    templateUrl: 'views/404.html'
+  });
 
+});
 
